02c5b89c0c7c052d8191b09efce2d3c24742be81
Bryan Finlayson <bryan.finlayson@metova.com>
2018-07-02 17:28:15 -0500

    add cities controller, index and show methods

---
+++ CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok(new JsonResult(Models.CitiesDataStore.Current.Cities));
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var result = Models.CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+    }
+}
